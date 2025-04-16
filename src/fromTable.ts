930c83442b1d3a2be3466fe48174c25f39b365b9
Matthieu Viry <matthieu.viry@cnrs.fr>
2025-04-16 14:21:44 +0200

    feat: implement fromTable and toTable

--- src/fromTable.ts
+++ src/fromTable.ts
@@ -1,3 +1,37 @@
-const fromTable = () => {};
+import { Parser } from '@json2csv/plainjs';
+import { gdal } from './init';
+import {
+  type SupportedTabularFormat,
+  supportedTabularFormats,
+} from './supportedFormats';
+
+const fromTable = async (
+  table: Record<string, unknown>[],
+  format: SupportedTabularFormat,
+): Promise<Blob> => {
+  // Check the various parameters
+  if (!supportedTabularFormats.includes(format)) {
+    throw new Error(`Unsupported format! ${format}`);
+  }
+
+  const parser = new Parser();
+  const csv = parser.parse(table);
+  const csvFile = new File([csv], 'file.csv', { type: 'text/csv' });
+  // Open the CSV file
+  const input = await gdal!.open(csvFile);
+  // Options for ogr2ogr
+  const options = ['-f', format];
+  const output = await gdal!.ogr2ogr(input.datasets[0], options);
+  const bytes = await gdal!.getFileBytes(output);
+  await gdal!.close(input as never);
+  // Mime type given the input format
+  const mimeType =
+    format === 'ODS'
+      ? 'application/vnd.oasis.opendocument.spreadsheet'
+      : format === 'XLSX'
+        ? 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        : '';
+  return new Blob([bytes], { type: mimeType });
+};
 
 export default fromTable;
--- src/toTable.ts
+++ src/toTable.ts
@@ -1,3 +1,69 @@
-const toTable = () => {};
+import toGeoJSON from './toGeoJSON';
+
+type ToTableOptions = {
+  // The name of the table to extract (mandatory if the dataset contains
+  // multiple sheets).
+  tableName?: string;
+};
+
+const toTable = async (file: File, options: ToTableOptions = {}) => {
+  const opts = options.tableName ? { layerName: options.tableName } : {};
+  const layer = await toGeoJSON(file, opts);
+
+  if (!layer || !('features' in layer) || layer.features.length < 1) {
+    throw new Error('An error occurred or the table is empty');
+  }
+
+  const columnsBefore = Object.keys(layer.features[0].properties!);
+  const columnsAfter = columnsBefore.slice();
+
+  // We want to take care of the case where the column names are not correctly
+  // identified and became Field1, Field2, etc.
+  // In such cases, we need to take the first data row as the header row.
+  if (
+    JSON.stringify(columnsAfter)
+    === JSON.stringify(
+      Array.from({ length: columnsAfter.length }).map(
+        (d, i) => `Field${i + 1}`,
+      ),
+    )
+  ) {
+    const firstRow = layer.features[0].properties;
+    for (let i = 0; i < columnsBefore.length; i += 1) {
+      // @ts-expect-error abc
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      columnsAfter[i] = firstRow[columnsBefore[i]];
+    }
+
+    // We remove the first row from the data
+    layer.features.shift();
+
+    // We update the properties of the features
+    layer.features.forEach((f) => {
+      const properties = {};
+      for (let i = 0; i < columnsBefore.length; i += 1) {
+        // @ts-expect-error abc
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        properties[columnsAfter[i]] = f.properties[columnsBefore[i]];
+      }
+      f.properties = properties;
+    });
+  }
+
+  const rows = layer.features.map((f) => f.properties);
+
+  // Remove lines at the end of the file that only contain empty cells
+  let lastDataRowIndex = rows.length - 1;
+  while (
+    lastDataRowIndex >= 0
+    // @ts-expect-error abc
+    && columnsAfter.every((c) => rows[lastDataRowIndex][c] === undefined)
+  ) {
+    lastDataRowIndex -= 1;
+  }
+
+  // Return the cleaned dataset
+  return rows.slice(0, lastDataRowIndex + 1);
+};
 
 export default toTable;
