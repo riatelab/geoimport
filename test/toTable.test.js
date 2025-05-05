28669ec9a09bc14e5a3fc66dc848f174d6afa844
Matthieu Viry <matthieu.viry@cnrs.fr>
2025-05-05 12:06:28 +0200

    test: new test for toTable function

--- test/toTable.test.js
+++ test/toTable.test.js
@@ -22,4 +22,39 @@ QUnit.module('toTable', (hooks) => {
       'The returned table has the correct number of entries',
     );
   });
+
+  QUnit.test('from ODS file with one sheet', async (assert) => {
+    await loadLib();
+    const xlsxFile = new File([xlsx_blob_one_sheet_bad_headers], 'test.xlsx', {
+      type: xlsx_blob_one_sheet_bad_headers.type,
+    });
+
+    const res = await geoimport.toTable(xlsxFile);
+
+    assert.equal(
+      res.every((a) => typeof a === 'object'),
+      true,
+      'The returned table is an Array of Objects',
+    );
+    assert.equal(
+      res.every((a) => Object.keys(a).length === 3),
+      true,
+      'The returned table has the correct number of columns',
+    );
+    assert.equal(
+      res.length,
+      361,
+      'The returned table has the correct number of entries',
+    );
+    assert.equal(
+      Object.keys(res[0])[0],
+      'Local Authority',
+      'Header 1 have been detected correctly',
+    );
+    assert.equal(
+      Object.keys(res[0])[1],
+      'Area Code',
+      'Header 2 have been detected correctly',
+    );
+  });
 });
